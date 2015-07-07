279d66b3e9d8caf5e52b71966457ad61ac82674a
Romain <romain@cozycloud.cc>
2015-07-07 10:24:52 +0200

    built

--- build/server/models/event.js
+++ build/server/models/event.js
@@ -215,7 +215,9 @@ Event.migrateAll = function(callback) {
     } else {
       return async.eachLimit(events, 10, function(event, done) {
         return event.migrateDoctype(function() {
-          return event.patchTag(done);
+          return event.patchTag(function() {
+            return setImmediate(done);
+          });
         });
       }, callback);
     }
