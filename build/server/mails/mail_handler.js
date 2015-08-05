5eea122989accd6acde5cc947ec6d6e220ecafc3
Frank Rousseau <frank.rousseau@cozycloud.cc>
2015-08-05 12:24:28 +0200

    build

--- build/server/mails/mail_handler.js
+++ build/server/mails/mail_handler.js
@@ -55,9 +55,9 @@ module.exports.sendInvitations = function(event, dateChanged, callback) {
         description: event.description
       });
       url = domain + "public/calendar/events/" + event.id;
-      date = event.formatStart(dateFormat);
-      dateFormat = localization.t(dateFormatKey);
       dateFormatKey = event.isAllDayEvent() ? 'email date format allday' : 'email date format';
+      dateFormat = localization.t(dateFormatKey);
+      date = event.formatStart(dateFormat);
       _ref = event.toJSON(), description = _ref.description, place = _ref.place;
       place = (place != null ? place.length : void 0) > 0 ? place : "";
       templateOptions = {
