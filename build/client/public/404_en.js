c4186febad85793fed595c1b9728f663a0975e3e
Romain <romain@cozycloud.cc>
2017-01-05 11:17:34 +0100

    rebuilt

--- build/client/public/404_en.js
+++ build/client/public/404_en.js
@@ -3,5 +3,5 @@ var buf = [];
 var jade_mixins = {};
 var jade_interp;
 
-buf.push("<!DOCTYPE html><html><head><title>404 - Event not found</title><link rel=\"stylesheet\" type=\"text/css\" href=\"stylesheets/app-15d9899c.css\" media=\"all\"></head><body class=\"error-404\"><div class=\"container\"><div class=\"row\"><div class=\"col-lg-12 error-frame\"><p class=\"headline\">This event is unaccessible</p><p>Either it does not exist</p><p>or</p><p>You are not invited ;)</p></div></div></div></body></html>");;return buf.join("");
+buf.push("<!DOCTYPE html><html><head><title>404 - Event not found</title><link rel=\"stylesheet\" type=\"text/css\" href=\"stylesheets/app-d71c3cd3.css\" media=\"all\"></head><body class=\"error-404\"><div class=\"container\"><div class=\"row\"><div class=\"col-lg-12 error-frame\"><p class=\"headline\">This event is unaccessible</p><p>Either it does not exist</p><p>or</p><p>You are not invited ;)</p></div></div></div></body></html>");;return buf.join("");
 }
--- build/client/public/404_fr.js
+++ build/client/public/404_fr.js
@@ -3,5 +3,5 @@ var buf = [];
 var jade_mixins = {};
 var jade_interp;
 
-buf.push("<!DOCTYPE html><html><head><title>404 - Événement non trouvé</title><link rel=\"stylesheet\" type=\"text/css\" href=\"stylesheets/app-15d9899c.css\" media=\"all\"></head><body class=\"error-404\"><div class=\"container\"><div class=\"row\"><div class=\"col-lg-12 error-frame\"><p class=\"headline\">Cet événement n'est pas accessible</p><p>Il n'existe pas</p><p>ou</p><p>Vous n'y êtes pas convié ;)</p></div></div></div></body></html>");;return buf.join("");
+buf.push("<!DOCTYPE html><html><head><title>404 - Événement non trouvé</title><link rel=\"stylesheet\" type=\"text/css\" href=\"stylesheets/app-d71c3cd3.css\" media=\"all\"></head><body class=\"error-404\"><div class=\"container\"><div class=\"row\"><div class=\"col-lg-12 error-frame\"><p class=\"headline\">Cet événement n'est pas accessible</p><p>Il n'existe pas</p><p>ou</p><p>Vous n'y êtes pas convié ;)</p></div></div></div></body></html>");;return buf.join("");
 }
